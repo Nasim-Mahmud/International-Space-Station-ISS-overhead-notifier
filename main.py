2dd0130a37d68b92a435ce79f00442d83d36e11c
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-18 15:21:38 +0600

    Comparing the ISS position to my lat long and checking if it's dark outside.

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from datetime import datetime as dt
 MY_LAT = 23.810331
 MY_LONG = 90.412521
 
-# Importing API endpoint
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
 
-data = response.json()
-iss_longitude = float(data["iss_position"]["longitude"])
-iss_latitude = float(data["iss_position"]["latitude"])
+def iss_overhear():
+    # Importing API endpoint
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+
+    data = response.json()
+    iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    # If the ISS is close to my current position
+    if (MY_LAT - 5) <= iss_latitude <= (MY_LAT + 5) and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
 
 # Sunset time
 parameters = {
@@ -19,28 +25,22 @@ parameters = {
     "formatted": 0,
 }
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
 
-# Time will be shown as UTC time zone
-data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+def is_dark_outside():
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
 
-# Optional: Sunrise and sunset time based on my location (Which is Dhaka, Bangladesh: GMT +6)
-sunrise_in_my_country = -24 + int(sunrise) + 6
-sunset_in_my_country = int(sunset) + 6
-# print(sunrise_in_my_country)
-# print(sunset_in_my_country)
-time_now = dt.now()
-# print(time_now.hour)
+    # Time will be shown as UTC time zone
+    data = response.json()
+    sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+    sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-# If the ISS is close to my current position
-if (MY_LAT-5) <= iss_latitude <= (MY_LAT+5) and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
-    print("Yes")
-else:
-    print("No")
+    # Optional: Sunrise and sunset time based on my location (Which is Dhaka, Bangladesh: GMT +6)
+    sunrise_in_my_country = -24 + int(sunrise) + 6
+    sunset_in_my_country = int(sunset) + 6
+    time_now = dt.now().hour
+    if time_now >= sunset_in_my_country or time_now <= sunrise_in_my_country:
+        return True
 
-# And it is dark
 # Then send me an email to tell me to look up.
 # Run the code in every 60 seconds.
