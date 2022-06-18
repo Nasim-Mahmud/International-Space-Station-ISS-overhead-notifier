2fcff7c3a2e15ef201c241e7b8c5d4e4935a5fcf
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-18 14:53:17 +0600

    Finding the sunrise ans sunset time to make sure the sky is dark enough to see the ISS.

--- main.py
+++ main.py
@@ -32,5 +32,6 @@ sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
 sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
 print(sunrise)
-# time_now = dt.now()
-# print(time_now)
+print(sunset)
+time_now = dt.now()
+print(time_now.hour)
