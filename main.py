060e149faa5bd9134bbc9eeb22a2f2abedcf84ac
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-18 15:03:48 +0600

    Finding the sunrise ans sunset time to make sure the sky is dark enough to see the ISS.

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from datetime import datetime as dt
 MY_LAT = 23.810331
 MY_LONG = 90.412521
 
-# # Importing API endpoint
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
-#
-# data = response.json()
-# longitude = data["iss_position"]["longitude"]
-# latitude = data["iss_position"]["latitude"]
-#
-# iss_position = (latitude, longitude)
-# print(iss_position)
+# Importing API endpoint
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (latitude, longitude)
+print(iss_position)
 
 # Sunset time
 
@@ -31,7 +31,15 @@ data = response.json()
 sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
 sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-print(sunrise)
-print(sunset)
+# Optional: Sunrise and sunset time based on my location (Which is Dhaka, Bangladesh: GMT +6)
+sunrise_in_my_country = -24 + int(sunrise) + 6
+sunset_in_my_country = int(sunset) + 6
+print(sunrise_in_my_country)
+print(sunset_in_my_country)
 time_now = dt.now()
 print(time_now.hour)
+
+# If the ISS is close to my current position
+# And it is dark
+# Then send me an email to tell me to look up.
+# Run the code in every 60 seconds.
