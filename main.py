584a08c3424efe6896a00fe50c8c44fc5d93bee9
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-18 15:13:16 +0600

    Comparing the ISS position to my lat long.

--- main.py
+++ main.py
@@ -9,14 +9,10 @@ response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-
-iss_position = (latitude, longitude)
-print(iss_position)
+iss_longitude = float(data["iss_position"]["longitude"])
+iss_latitude = float(data["iss_position"]["latitude"])
 
 # Sunset time
-
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
@@ -34,12 +30,17 @@ sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 # Optional: Sunrise and sunset time based on my location (Which is Dhaka, Bangladesh: GMT +6)
 sunrise_in_my_country = -24 + int(sunrise) + 6
 sunset_in_my_country = int(sunset) + 6
-print(sunrise_in_my_country)
-print(sunset_in_my_country)
+# print(sunrise_in_my_country)
+# print(sunset_in_my_country)
 time_now = dt.now()
-print(time_now.hour)
+# print(time_now.hour)
 
 # If the ISS is close to my current position
+if (MY_LAT-5) <= iss_latitude <= (MY_LAT+5) and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+    print("Yes")
+else:
+    print("No")
+
 # And it is dark
 # Then send me an email to tell me to look up.
 # Run the code in every 60 seconds.
