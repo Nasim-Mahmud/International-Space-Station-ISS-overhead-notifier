334ad614674518a34093c75b6bab4731223e674a
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-10 21:24:34 +0600

    Fixing bugs.

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def iss_overhear():
     if (MY_LAT - 5) <= iss_latitude <= (MY_LAT + 5) and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
         return True
 
+
 # Sunset time
 parameters = {
     "lat": MY_LAT,
